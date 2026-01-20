352608d91567f8d86a00632342ec68550160a3c0
Shiburaj Pappu <69638955+shiburaj@users.noreply.github.com>
2026-01-20 23:05:04 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# AIM: Task List Manager
+# Coder:
+# Date:
+
+print("--- Task List Manager ---")
+tasks = ["Sleep", "Getup", "Brush"]
+print(f"Original Tasks: {tasks}")
+
+# Write your code here
+# TODO: Add & Print new Task from user
+
+# TODO: Edit & Print task selected by User
+
+# TODO: Remove & Print a Task selected by User
+
+# TODO: Sort & Print the Tasks
